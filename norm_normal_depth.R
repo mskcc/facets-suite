49625dd8390d45a18dc8e055641f3b8775a6284e
Alexander Penson <pensona@cbio.mskcc.org>
2017-06-20 15:51:42 -0400

    works with snp-pileup

--- norm_normal_depth.R
+++ norm_normal_depth.R
@@ -23,34 +23,41 @@ library(data.table)
 
 
 
-normalize_facets_depth <- function(filename){
+normalize_facets_depth <- function(filename, out_filename){
   countsMerged <- suppressWarnings(fread(paste0('gunzip --stdout ', filename), showProgress = FALSE))
-  countsMerged$Chrom <- factor(countsMerged$Chrom, levels=c(1:22, "X", "Y"))
-  dt <- with(countsMerged[TUM.DP>0][order(NOR.DP)], 
-             data.table(Chrom, 
-                        Pos, 
-                        TUM.DP,
-                        NOR.DP,
-                        M=log2(NOR.DP), 
-                        A=log2(TUM.DP/NOR.DP)
-             )
-  )
+  countsMerged$Chromosome <- factor(countsMerged$Chromosome, levels=c(1:22, "X", "Y"))
+  countsMerged[, File1DP := File1R + File1A]
+  countsMerged[, File2DP := File2R + File2A]
+  dt <- countsMerged[
+    File2DP>0 & File1DP>0][
+      order(File1DP)][, 
+                      .(Chromosome, 
+                        Position, 
+                        File1DP,
+                        File2DP,
+                        M=log2(File1DP), 
+                        A=log2(File2DP/File1DP)
+                      )
+]
 
   
   lx <- with(dt, lowess(M, A)) ### variables in "MA plot"
   dt[,A.lowess := lx$y]
   A.mean <- mean(dt$A, na.rm=T)
   dt[,A.norm := A - A.lowess + A.mean]
-  dt[,norm.factor := NOR.DP*2^A.norm / TUM.DP]
+  dt[,norm.factor := File1DP*2^A.norm / File2DP]
   
   countsMerged_merge <- merge(countsMerged,
-                              dt[,c("Chrom", "Pos", "norm.factor"), with=F],
-                              by=c("Chrom", "Pos"),
+                              dt[,c("Chromosome", "Position", "norm.factor"), with=F],
+                              by=c("Chromosome", "Position"),
                               all.x=T)
-  for (i in 5:13){ ### loop over TUM.* columns
+  for (i in 9:12){ ### loop over TUM.* columns
     set(countsMerged_merge, NULL, i, countsMerged_merge[[i]] * countsMerged_merge$norm.factor) ### mulitply by norm.factor
     countsMerged_merge[[i]][is.na(countsMerged_merge[[i]])] <- 0 ### retain TUM.DP == 0
   }
+  countsMerged_merge <- countsMerged_merge[File1DP > 1]
+  countsMerged_merge[,File1DP := NULL]
+  countsMerged_merge[,File2DP := NULL]
   countsMerged_merge[,norm.factor := NULL]
 
   #### DIRECT GZIP OUTPUT DOESN'T WORK ??
@@ -59,18 +66,19 @@ normalize_facets_depth <- function(filename){
   ## gz = gzfile(output_filename,"w");
   ## write.maf(countsMerged_merge, gz)
 
-  output_filename <- gsub(".dat.gz$", ".norm_normal_depth.dat", filename)
+  # output_filename <- gsub(".dat.gz$", ".norm_normal_depth.dat", filename)
 
-  write.table(countsMerged_merge, file=stdout(), 
+  write.table(countsMerged_merge, file=gzfile(out_filename), 
               quote = F, 
               col.names = T, 
               row.names = F, 
-              sep = "\t")
+              sep = ",")
 ###  system(paste0("gzip -f ", output_filename))
 }
 
 if(!interactive()){
   args <- commandArgs(TRUE)
-  filename <- args[1]
-  normalize_facets_depth(filename)
+  filename <- args[[1]]; args <- args[-1]
+  out_filename <- args[[1]]; args <- args[-1]
+  normalize_facets_depth(filename, out_filename)
 }
