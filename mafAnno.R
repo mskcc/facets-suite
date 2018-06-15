a53c872d6aa171b261af59c03da712a8473d781e
kpjonsson <philip.jonsson@gmail.com>
2018-06-15 11:42:19 -0400

    Fixes to mafAnno.R

--- mafAnno.R
+++ mafAnno.R
@@ -2,11 +2,16 @@
 
 # Functions ------------------------------------------------------------------------------------------------------------
 integer_cn_table = function(diplogr, fit) {
-    
+
     df = fit$cncf
-    if (!'loc.start' %in% names(df) | !'loc.end' %in% names(df)) {
-        df$loc.start = df$start
-        df$loc.end = df$end
+    if (!'loc.start' %in% names(df) | !'loc.end' %in% names(df)) { # this should capture all differently formatted outputs
+        if (all(c('start', 'end') %in% names(fit$cncf))) {
+                df$loc.start = df$start
+                df$loc.end = df$end
+        } else {
+                df$loc.start = fit$start
+                df$loc.end = fit$end
+        }
     }
     df$chrom[which(df$chrom == 23)] = 'X'
     df$chrom = factor(df$chrom)
@@ -22,13 +27,13 @@ integer_cn_table = function(diplogr, fit) {
                     ploidy = fit$ploidy,
                     purity = fit$purity,
                     dipLogR = diplogr)
-    
+
     setkey(dt, chrom, loc.start, loc.end)
     dt
 }
 
 annotate_maf_with_facets_cf_tcn_lcn = function(maf, diplogr, fit, iTumor_Sample_Barcode = NULL) {
-    
+
     maf_cols = colnames(maf)
     maf$Chromosome = factor(maf$Chromosome)
     setkey(maf, Chromosome, Start_Position, End_Position)
@@ -36,10 +41,10 @@ annotate_maf_with_facets_cf_tcn_lcn = function(maf, diplogr, fit, iTumor_Sample_
 
     # check for duplicate columns
     if (any(duplicated(names(maf)))) {
-        warning("duplicate columns removed from maf file")
+        warning("Duplicate columns removed from MAF file")
         maf[, which(duplicated(names(maf))) := NULL, with = F]
     }
-    
+
     if (is.null(iTumor_Sample_Barcode)) {
         maf_ann = foverlaps(maf, dt, mult = "first", nomatch = NA)
     } else {
@@ -52,7 +57,7 @@ annotate_maf_with_facets_cf_tcn_lcn = function(maf, diplogr, fit, iTumor_Sample_
 }
 
 ccf.likelihood = function(purity, absCN, alt_allele, coverage, copies) { #From McGranahan_and_Swanton_2015
-    
+
     CCFs = seq(0.001,1,0.001)
     vac.ccf  = function(CCF, purity, absCN){purity * CCF * copies / (2*(1 - purity) + purity * absCN)}
     probs = sapply(CCFs, function(c){dbinom(alt_allele, coverage, vac.ccf(c, purity, absCN))})
@@ -73,7 +78,7 @@ ccf.likelihood = function(purity, absCN, alt_allele, coverage, copies) { #From M
 }
 
 main = function(maf, facets_files, file_type = 'Rdata'){
-    
+
     maf = as.data.table(maf)
     maf_Tumor_Sample_Barcodes = unique(maf$Tumor_Sample_Barcode)
 
@@ -88,7 +93,7 @@ main = function(maf, facets_files, file_type = 'Rdata'){
 
     idi = intersect(names(facets_files), maf_Tumor_Sample_Barcodes)
     maf = maf[maf$Tumor_Sample_Barcode %in% idi]
-    
+
     if (file_type == 'Rdata') {
         maf_list = lapply(idi, function(x) {
             load(facets_files[x])
@@ -108,13 +113,13 @@ main = function(maf, facets_files, file_type = 'Rdata'){
             annotate_maf_with_facets_cf_tcn_lcn(maf, out$dipLogR, fit, x)
         })
     }
-    
+
     if (length(no.facets)) { maf_list = c(maf_list, list(no.facets.data)) }
     maf = rbindlist(maf_list,fill = T)
 
     maf[, t_alt_count := as.numeric(t_alt_count)]
     maf[, t_ref_count := as.numeric(t_ref_count)]
-    
+
     maf[, c("ccf_Mcopies", "ccf_Mcopies_lower", "ccf_Mcopies_upper", "ccf_Mcopies_prob95", "ccf_Mcopies_prob90") :=
             ccf.likelihood(purity, # CCF estimates if mutation on mcn, cncf algorithm
                                          tcn,
@@ -158,7 +163,7 @@ suppressPackageStartupMessages({
     })
 
 if (!interactive()) {
-    
+
     parser=ArgumentParser()
     parser$add_argument('-m', '--maf', type='character', help='file name of maf file to be annotated')
     parser$add_argument('-f', '--facets_files', type = 'character',
@@ -166,15 +171,15 @@ if (!interactive()) {
     parser$add_argument('-o', '--out_maf', type = 'character', help = 'file name of CN annotated maf')
     parser$add_argument('-c', '--save_comments', action = 'store_true', default = TRUE, help = 'Retains comments in file header')
     args=parser$parse_args()
-    
+
     maf_file = args$maf
     facets_samples_file = args$facets_files
     output_maf_file = args$out_maf
     save_comments = args$save_comments
-    
+
     maf = fread(maf_file, skip = 'Hugo_Symbol')
     facets_samples = fread(facets_samples_file)
-    
+
     if ('Rdata_filename' %in% names(facets_samples)) { # Rdata input
         facets_files = with(facets_samples, structure(Rdata_filename, .Names = Tumor_Sample_Barcode))
         maf = main(maf, facets_files, 'Rdata')
@@ -182,12 +187,12 @@ if (!interactive()) {
         facets_files = with(facets_samples, structure(cncf_filename, .Names = Tumor_Sample_Barcode))
         maf = main(maf, facets_files, 'cncf')
     }
-    
+
     if (save_comments) {
         header = readLines(maf_file, n = 10)
         header = header[unlist(lapply(header, function(x) substr(x,1,1)=='#'))]
         out_file = file(output_maf_file, open = 'wt')
-        for (i in 1:length(header)) cat(header[i], '\n', file = out_file, append = T)
+        if (length(header) > 0) for (i in 1:length(header)) cat(header[i], '\n', file = out_file, append = T)
         write.table(maf, file = out_file, quote = F, col.names = T, row.names = F, sep = "\t")
         close(out_file)
     } else {
@@ -195,5 +200,3 @@ if (!interactive()) {
                     quote = F, col.names = T, row.names = F, sep = "\t")
     }
 }
-    
-
