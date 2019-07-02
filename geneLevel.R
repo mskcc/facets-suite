ddb4a483a386034052ddcb51b17b0c0a7c6e79e4
kpjonsson <philip.jonsson@gmail.com>
2019-07-02 15:03:43 -0400

    better solution

--- geneLevel.R
+++ geneLevel.R
@@ -261,9 +261,9 @@ get_gene_level_calls <- function(cncf_files,
     exome_bed <- suppressWarnings(fread(paste0('grep -v "^@" ',getSDIR(),"/data/Homo_sapiens.GRCh37.75.canonical_exons.bed")))
     setnames(exome_bed, c("chr", "start", "end", "name","blank","strand"))
     setkey(exome_bed, chr, start, end)
-    exome_bed = exome_bed[!name %like% 'ENSG00000217792'] # manual removal of pseudogene
     exome_bed = exome_bed[chr %in% c(seq(1, 22), 'X')] # retain only autosomes and X
     exome_bed[, Hugo_Symbol := gsub(":.*$", "", name)]
+    exome_bed[, Hugo_Symbol := ifelse(name %like% 'ENSG00000217792', 'LSP1P1', Hugo_Symbol)] # manual fix of bad annotation
     rm_genes = unique(exome_bed[, list(Hugo_Symbol, chr)])[, .N, keyby = Hugo_Symbol][N > 1] # these genes are mapping to multiple chromosomes
     exome_bed = exome_bed[Hugo_Symbol %ni% rm_genes$Hugo_Symbol]
     cross <- foverlaps(exome_bed, concat_cncf_txt, nomatch=NA)
