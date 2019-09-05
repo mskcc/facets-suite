2d8e85e96fd8fe85f03f7d34d088ea71b4c277be
kpjonsson <philip.jonsson@gmail.com>
2019-09-05 17:00:10 -0400

    fix test

--- tests/testthat/test-gene-level-changes.R
+++ tests/testthat/test-gene-level-changes.R
@@ -10,6 +10,6 @@ test_that("gene-level function works", {
     
     test_names = c('gene', 'chrom', 'gene_start', 'gene_end', 'seg', 'median_cnlr_seg', 'segclust', 'seg_start',
                    'seg_end', 'cf', 'tcn', 'lcn', 'seg_length', 'mcn', 'genes_on_seg', 'gene_snps', 'gene_het_snps',
-                   'spans_segs', 'cn_state', 'mean_cnlr', 'pval', 'fold_change')
+                   'spans_segs', 'cn_state')
     expect_true(all(test_names %in% names(test_gene_level)))
 })
